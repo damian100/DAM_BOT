dc80632cd4cdf67f263c659953ff4d73da1ca407
damian100 <lsu011127@gmail.com>
2020-07-20 22:23:09 +0900

    esd

--- src/main/java/damian/dam/bot/DMain.java
+++ src/main/java/damian/dam/bot/DMain.java
@@ -110,13 +110,14 @@ public class DMain extends ListenerAdapter{
         JDA jda = event.getJDA();
 
         ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("--whitelisted-ips='58.121.104.253'");
         //options.addArguments("--start-maximized");            // 전체화면으로 실행
         options.addArguments("--disable-popup-blocking");    // 팝업 무시
         options.addArguments("--disable-default-apps");     // 기본앱 사용안함
         options.addArguments("--no-sandbox");     //
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--verbose");
-        options.addArguments("--whitelisted-ips=''");
         // WebDriver 객체 생성
         ChromeDriver driver = new ChromeDriver(options);
         // 빈 탭 생성
